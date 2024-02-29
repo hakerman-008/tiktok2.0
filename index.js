95550a7639bc8fb95325b4f37110e2669b293158
hakerman <142625808+hakerman-008@users.noreply.github.com>
2024-02-29 14:19:24 +0545

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,17 @@ app.get('/tiktok', async (req, res) => {
     const apiKey = 'vrGjIdJL';
 
     const apiUrl = `https://api.betabotz.eu.org/api/search/tiktoks?query=${query}&apikey=${apiKey}`;
-    const response = await axios.get(apiUrl);
+
+    // Set appropriate headers including User-Agent
+    const headers = {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+      'Accept': 'application/json', // Add more headers if required
+    };
+
+    const response = await axios.get(apiUrl, { headers });
 
     if (response.data.status && response.data.result.data.length > 0) {
-     
       const playUrls = response.data.result.data.map(video => video.play);
-
       res.json({ urls: playUrls });
     } else {
       res.status(404).json({ error: 'No TikTok videos found for the provided query' });
